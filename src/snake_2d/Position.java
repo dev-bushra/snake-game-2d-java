42bd7665a35c5a8c4d250cda6f30404cd84c66b1
Bushra Aboubida Ahmed <bushragtv@gmail.com>
2022-04-15 01:19:45 +0200

    ..

---
+++ src/snake_2d/Position.java
@@ -0,0 +1,13 @@
+/*      Develop By : Bushra Aboubida Ahmed          */
+package snake_2d;
+ 
+public class Position {
+ 
+    int x, y;
+ 
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+ 
+}
